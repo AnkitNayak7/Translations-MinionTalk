1099b3469806f9bbb1986c52db37d61aa24d9a1a
Ankit Nayak <ankit.nyk2331@gmail.com>
2021-11-10 23:38:55 +0530

    Taking input and Console
    Taking input and Console

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 var btnTraslate = document.querySelector("#btn-translate")
+var txtInput = document.querySelector("#txt-input")
+
+function clickHandler(){
+    console.log("Clicked!");
+    console.log("input", txtInput.value)
+
+}
+
+btnTraslate.addEventListener("click", clickHandler)
 
 
-btnTraslate.addEventListener("click", function clickEventHandler(){
-    console.log("Clicked!")
-})
+
+
+
+
